87352be973993d04ab4a0c191d9efeb2471a8b1f
Smrutirekha Behera <smrutirekha414@gmail.com>
2022-10-06 22:51:05 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
 import React from "react";
-import Covid from "./components/covid";
-import Netflix from "./components/Netflix";
-//import "./index.css";
-//import Project1 from "./components/Project1";
 import Netmine from "./components/Netmine";
 
 function App() {
